0b563f2ae59ba57cb791458ed797b2a6dcad2960
Sérgio Souza Costa <sergio.costa@ufma.br>
2025-06-24 11:14:53 -0300

    incluido stmt em ast

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 lazy val root = (project in file(".")).
   settings(
     inThisBuild(List(
-      organization := "com.example",
+      organization := "br.ufma.ecp",
       scalaVersion := "3.5.2"
     )),
     name := "slox"
--- src/main/scala/br/ufma/ecp/slox/Ast.scala
+++ src/main/scala/br/ufma/ecp/slox/Ast.scala
@@ -8,3 +8,10 @@ object Expr:
   case class Grouping(expression: Expr) extends Expr
   case class Literal(value: Option[Any]) extends Expr
   case class Unary(operator: Token, right: Expr) extends Expr
+
+
+sealed trait Stmt
+
+object Stmt:
+  case class Expression(expr: Expr) extends Stmt
+  case class Print(expr: Expr) extends Stmt
